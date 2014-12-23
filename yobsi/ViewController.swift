ae079137e149a97c03930dc20e8326cf19cb521e
onix <alfre.arias@gmail.com>
2014-12-23 17:24:54 -0400

    Initial Commit

---
+++ yobsi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yobsi
+//
+//  Created by onix on 12/23/14.
+//  Copyright (c) 2014 xiobit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
