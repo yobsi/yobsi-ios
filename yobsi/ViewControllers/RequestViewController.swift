729a2b237b100405e2d75063256fdd533d9d6cbe
onix <alfre.arias@gmail.com>
2014-12-29 15:09:46 -0400

    some UI development

---
+++ yobsi/ViewControllers/RequestViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RequestViewController.swift
+//  yobsi
+//
+//  Created by onix on 12/29/14.
+//  Copyright (c) 2014 xiobit. All rights reserved.
+//
+
+import UIKit
+
+class RequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
